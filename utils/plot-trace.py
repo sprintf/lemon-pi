e694dc974b98e14f5d0969ca4f975e7f9265226a
sprintf <paul@normingtons.org>
2020-12-30 17:55:14 -0800

    little utility for plotting to google maps for diagnostic purposes. (#10)
    You need a google api key to make this work ... it's not included.

---
+++ utils/plot-trace.py
@@ -0,0 +1,21 @@
+import csv
+import gmplot
+import secrets
+
+gmap = gmplot.GoogleMapPlotter(37.928, -122.299, 16)
+gmap.apikey=secrets.gmap_apikey
+
+with open("../traces/trace-1608347418.csv") as csvfile:
+    points = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    x = 0
+    for point in points:
+        x += 1
+        if x % 10 == 0:
+            label = "hdg:{} spd:{}".format(int(point[3]),"?")
+            gmap.marker(point[1], point[2], label=label)
+
+gmap.draw("mymap.html")
+
+
+
+
