5ba14bab140217918e7b899e4d973fd49cbc57d5
sprintf <paul@normingtons.org>
2021-01-08 22:17:47 -0800

    adds tests to make sure that the config actually works

---
+++ config/test_settings.py
@@ -0,0 +1,3 @@
+
+# the size of the vehicle's fuel tank
+FUEL_CAPACITY_US_GALLONS = 10.5
---
+++ tests/maf_analyzer_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from maf_analyzer import MafAnalyzer
+
+from python_settings import settings
+import config.test_settings as my_local_settings
+
+settings.configure(my_local_settings)
+
+
+class MafTestCase(unittest.TestCase):
+
+    def test_initial_fuel_remaining(self):
+        ma = MafAnalyzer(None)
+        self.assertEqual(-1, ma.get_fuel_percent_remaining())
+
+    def test_no_fuel_remaining(self):
+        ma = MafAnalyzer(None)
+        ma.total_fuel_used_ml = 10.5 * 3785
+        self.assertEqual(0, ma.get_fuel_percent_remaining())
+
+
+if __name__ == '__main__':
+    unittest.main()
