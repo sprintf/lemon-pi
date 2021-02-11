3078e1e2210cbf43f994fc951df5015a6a7a7aa9
sprintf <paul@normingtons.org>
2021-02-10 22:02:29 -0800

    fixes up PYTHONPATH set PYTHONPATH='.' again to allow local scripts to work

--- start-car.sh
+++ start-car.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/pi/lemon-pi
 . venv/bin/activate
+export PYTHONPATH=`pwd`
 ./gen-protos.sh
 python3 lemon_pi/car/main.py
 
--- start-pit.sh
+++ start-pit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . venv/bin/activate
+export PYTHONPATH=`pwd`
 ./gen-protos.sh
 python3 lemon_pi/pit/main.py
 
