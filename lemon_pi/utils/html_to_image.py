f10478c506f938d0891fa12afab6da9e4cb65677
sprintf <paul@normingtons.org>
2021-02-11 23:14:52 -0800

    image magic clips images and makes them smaller stops using markdown on the tracks page and just uses anchors and images the good ol fashioned way speeds up html image generation by doing them all in one launch rather than relaunching for each image
    adds pypeteer and pillow to required libraries

--- lemon_pi/utils/html_to_image.py
+++ lemon_pi/utils/html_to_image.py
@@ -4,26 +4,27 @@ import asyncio
 import time
 from pyppeteer import launch
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("usage : [filename.html]")
     sys.exit(1)
 
-file = sys.argv[1]
-outfile = file.replace(".html", ".jpg")
+files = sys.argv[1:]
 
 
-async def main():
+async def main(files:[]):
     browser = await launch()
     page = await browser.newPage()
-    await page.goto('https://storage.googleapis.com/perplexus/public/{}'.format(file))
-    # we need to wait for the background to load
-    time.sleep(1)
-    await page.screenshot({'path': outfile, 'fullPage': 'false',
-                           'type': 'jpeg', 'quality': 50,
-                           'clip' : {
-                               'x': 0, 'y': 0,
-                               'width': 600, 'height': 600
-                           }})
+    for file in files:
+        outfile = file.replace(".html", ".jpg")
+        await page.goto('https://storage.googleapis.com/perplexus/public/{}'.format(file))
+        # we need to wait for the background to load
+        time.sleep(1)
+        await page.screenshot({'path': outfile, 'fullPage': 'false',
+                               'type': 'jpeg', 'quality': 50,
+                               'clip' : {
+                                   'x': 0, 'y': 0,
+                                   'width': 600, 'height': 600
+                               }})
     await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_until_complete(main(files))
