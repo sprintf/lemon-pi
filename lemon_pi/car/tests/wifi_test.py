753a24770f1003e21786201281df6552de3da10a
sprintf <paul@normingtons.org>
2021-02-28 21:35:59 -0800

    fix broken test

--- lemon_pi/car/tests/wifi_test.py
+++ lemon_pi/car/tests/wifi_test.py
@@ -5,7 +5,7 @@ from lemon_pi.car.wifi import WifiManager
 class WifiTest(unittest.TestCase):
 
     def test_command(self):
-        response = WifiManager().command(["echo", "hello"])
+        response = WifiManager._command(["echo", "hello"])
         self.assertEqual(response, "hello")
 
 
