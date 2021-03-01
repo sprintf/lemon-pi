0b970c91c5f13cb9b051c6dbd7b7aa3a2bb5a65a
sprintf <paul@normingtons.org>
2021-02-28 21:25:12 -0800

    adds logging to wifi

--- lemon_pi/car/wifi.py
+++ lemon_pi/car/wifi.py
@@ -2,31 +2,41 @@
 import subprocess
 import platform
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class WifiManager:
 
     @classmethod
-    def check_wifi_enabled(self):
+    def check_wifi_enabled(cls):
         if platform.system() == "Linux":
+            logger.info("detecting wifi...")
             response = WifiManager._command(['ifconfig', 'wlan0'])
             if response.find("RUNNING") and response.find("inet "):
+                logger.info("wifi operating")
                 return True
+            else:
+                logger.info("wifi is disabled")
         return False
 
     @classmethod
-    def disable_wifi(self):
+    def disable_wifi(cls):
         if platform.system() == "Linux":
             WifiManager._command(['sudo', 'rfkill', 'block', 'wifi'])
+            logger.info("wifi disabled")
 
     @classmethod
-    def enable_wifi(self):
+    def enable_wifi(cls):
         if platform.system() == "Linux":
             WifiManager._command(['sudo', 'rfkill', 'unblock', 'wifi'])
+            logger.info("waiting for wifi to enable...")
             time.sleep(7)
+            logger.info("that's enough waiting")
 
     @classmethod
-    def _command(self, args:[]):
+    def _command(cls, args:[]):
         result = subprocess.run(args, stdout=subprocess.PIPE)
         return result.stdout.decode("UTF-8").strip()
 
