73078a61219adacd5a78b3463cc730ac25a8d897
sprintf <paul@normingtons.org>
2021-03-28 19:50:45 -0700

    fixes unittests

--- lemon_pi/utils/send_fuel_level.py
+++ lemon_pi/utils/send_fuel_level.py
@@ -6,7 +6,7 @@ import time
 from python_settings import settings
 
 from lemon_pi.shared.generated.messages_pb2 import (
-    SetFuelLevel, ToPitMessage
+    ToPitMessage, ToCarMessage
 )
 from lemon_pi.shared.radio import Radio
 from lemon_pi.shared.usb_detector import UsbDetector
@@ -20,8 +20,8 @@ logging.basicConfig(format='%(asctime)s %(name)s %(message)s',
 if not "SETTINGS_MODULE" in os.environ:
     os.environ["SETTINGS_MODULE"] = "lemon_pi.config.local_settings_pit"
 
-if len(sys.argv) != 2:
-    print("usage : {} [0-100]".format(__file__))
+if len(sys.argv) != 3:
+    print("usage : {} [car-number] [0-100]".format(__file__))
     sys.exit(1)
 
 UsbDetector().init()
@@ -29,8 +29,9 @@ UsbDetector().init()
 radio = Radio(settings.RADIO_DEVICE, settings.RADIO_KEY, ToPitMessage())
 radio.start()
 
-msg = SetFuelLevel()
-msg.percent_full = int(sys.argv[1])
+msg = ToCarMessage()
+msg.set_fuel.percent_full = int(sys.argv[2])
+msg.set_fuel.car_number = sys.argv[1]
 
 radio.send_async(msg)
 
