715384bd18c258fbbb68cf37814bda3840213fdb
sprintf <paul@normingtons.org>
2021-08-06 21:04:41 -0700

    more button play

--- lemon_pi/car/button.py
+++ lemon_pi/car/button.py
@@ -1,19 +1,26 @@
 
 import RPi.GPIO as GPIO
 from event_defs import ButtonPressEvent
+from threading import Thread
+import time
 
 INPUT_PIN = 10
 
 
-class Button:
+class Button(Thread):
 
     def __init__(self):
+        Thread.__init__(self, daemon=True)
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         # might want to try bouncetime=200 if its very bouncy
-        GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=self.button_handler, bouncetime=200)
+        # GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=self.button_handler, bouncetime=200)
 
-    def button_handler(self, channel):
-        ButtonPressEvent.emit(button=0)
+    def run(self):
+        while True:
+            GPIO.wait_for_edge(INPUT_PIN, GPIO.RISING)
+            ButtonPressEvent.emit(button=0)
+            time.sleep(0.2)
+
 
--- lemon_pi/car/main.py
+++ lemon_pi/car/main.py
@@ -136,7 +136,7 @@ def init():
     gui.present_main_app()
 
     # bring in a button listener, to read the hardware button
-    button = Button()
+    Button().start()
 
     logger.info("awaiting location to choose track")
     while not gps.is_working() or gps.get_lat_long() == (0, 0):
