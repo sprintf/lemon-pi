da1327c121020fd84b5fe5d56cb2b8a910f33300
sprintf <paul@normingtons.org>
2021-08-07 09:50:36 -0700

    fine tuning button

--- lemon_pi/car/button.py
+++ lemon_pi/car/button.py
@@ -1,9 +1,11 @@
 
 import RPi.GPIO as GPIO
 from lemon_pi.car.event_defs import ButtonPressEvent
+import logging
 
 INPUT_PIN = 10
 
+logger = logging.getLogger(__name__)
 
 class Button():
 
@@ -12,9 +14,10 @@ class Button():
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         # might want to try bouncetime=200 if its very bouncy
-        GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=self.button_handler, bouncetime=200)
+        GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=self.button_handler, bouncetime=250)
 
     def button_handler(self, channel):
+        logger.info("button pressed")
         ButtonPressEvent.emit(button=0)
 
 
