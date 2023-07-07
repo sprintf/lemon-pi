4cd31d7892eebfba1e91012874615855174f7f8e
sprintf <paul@normingtons.org>
2023-07-07 16:15:04 -0700

    switch to using Playwright library for taking snapshot of the track

--- lemon_pi/utils/html_to_image.py
+++ lemon_pi/utils/html_to_image.py
@@ -1,31 +1,31 @@
 
-import sys
-import asyncio
 import time
-from pyppeteer import launch
+import sys
+from playwright.sync_api import sync_playwright
 
 if len(sys.argv) < 2:
     print("usage : [filename.html]")
     sys.exit(1)
 
-files = sys.argv[1:]
+def main(files:[]):
+    with sync_playwright() as browser_controller:
+        browser = browser_controller.chromium.launch()
+        for file in files:
+            print(f"processing {file}")
+            outfile = file.replace(".html", ".jpg")
+            page = browser.new_page()
+            page.goto('https://storage.googleapis.com/perplexus/public/{}'.format(file))
+            time.sleep(1)
+            page.screenshot(path=outfile,
+                            full_page=False,
+                            type="jpeg",
+                            quality=50,
+                            clip={
+                                       'x': 200, 'y': 0,
+                                       'width': 900, 'height': 900
+                                   },
+                            scale="device",
+                            )
 
-
-async def main(files:[]):
-    browser = await launch()
-    page = await browser.newPage()
-    for file in files:
-        print(f"processing {file}")
-        outfile = file.replace(".html", ".jpg")
-        await page.goto('https://storage.googleapis.com/perplexus/public/{}'.format(file))
-        # we need to wait for the background to load
-        time.sleep(1)
-        await page.screenshot({'path': outfile, 'fullPage': 'false',
-                               'type': 'jpeg', 'quality': 50,
-                               'clip' : {
-                                   'x': 0, 'y': 0,
-                                   'width': 600, 'height': 600
-                               }})
-    await browser.close()
-
-asyncio.get_event_loop().run_until_complete(main(files))
+if __name__ == "__main__":
+    main(sys.argv[1:])
